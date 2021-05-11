da3a4e659adbb9a631e6a07e4d9905541d7e955c
Yuke Wang <505008605@qq.com>
2021-05-10 22:21:51 -0700

    update with files

---
+++ 00_run-gemm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+
+M_N_K_list = [
+    128,
+    256,
+    384,
+    512,
+    640,
+    768,
+    896,
+    1024,
+]
+
+for m in M_N_K_list:
+    os.system("./bench_gemm {} {} {}".format(64, m, m))
